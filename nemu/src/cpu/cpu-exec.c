a4fcbfa8092368b492dd35de770cff812a4622f4
Hubert <hubert99203@gmail.com>
2024-03-12 02:54:19 +0800

    PA1.3: Accomplish Using macro-variable switch to enable watchpoint scanning.

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,8 +42,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
+#ifdef CONFIG_SCAN_WATCHPOINT
   // scan all watchpoints.
   scan_watchpoints();
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
