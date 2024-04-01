807e5af0ad42ad75282dd88a359286d5c2397122
Hubert <hubert99203@gmail.com>
2024-04-02 06:57:23 +0800

    NJU-ProjectN/nanos-lite ics2021 initialized
    NJU-ProjectN/nanos-lite 2a141760e31be246a7316942293a97873925bc2f Makefile: use header files in newlib-navy

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -9,7 +9,10 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
-  uintptr_t mepc, mcause, gpr[NR_REGS], mstatus;
+  uintptr_t gpr[NR_REGS];
+  uintptr_t mcause;
+  uintptr_t mstatus;
+  uintptr_t mepc;
   void *pdir;
 };
 
--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,9 +5,20 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+  // test the Context
+  // printf("General Purpose Registers:\n");
+  // for (int i = 0; i < NR_REGS; i++) {
+  //     printf("gpr[%d]: %x\n", i, c->gpr[i]);
+  // }
+  // printf("mcause: %x\n", c->mcause);
+  // printf("mstatus: %x\n", c->mstatus);
+  // printf("mepc: %x\n", c->mepc);
+  // printf("pdir: %p\n", c->pdir);
+
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 0xb : ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
